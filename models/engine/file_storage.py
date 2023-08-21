524136e1a040a0795fd387a49638c03d1c166eff
christophersesugh <christohybrid185@gmail.com>
2023-08-21 14:48:33 +0000

    refactor: update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    classes = {'BaseModel': BaseModel, 'User': User,
+                   'State': State, 'City': City, 'Amenity': Amenity,
+                   'Place': Place, 'Review': Review}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        return_all = {}
+
+        if cls:
+            if cls.__name__ in self.classes:
+                for key, val in self.__objects.items():
+                    if key.split('.')[0] == cls.__name__:
+                        return_all.update({key: val})
+        else:
+            return_all = self.__objects
+
+        return return_all
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
